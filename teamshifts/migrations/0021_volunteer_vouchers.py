b0c32103acdbd36515750f7f4542b8d6e053b471
MukundC25 <157853486+MukundC25@users.noreply.github.com>
2026-09-13 18:37:54 +0530

    fix migrations

--- teamshifts/migrations/0021_volunteer_vouchers.py
+++ teamshifts/migrations/0021_volunteer_vouchers.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
             fields=[
                 (
                     "id",
-                    models.AutoField(
+                    models.BigAutoField(
                         auto_created=True,
                         primary_key=True,
                         serialize=False,
@@ -58,7 +58,7 @@ class Migration(migrations.Migration):
             fields=[
                 (
                     "id",
-                    models.AutoField(
+                    models.BigAutoField(
                         auto_created=True,
                         primary_key=True,
                         serialize=False,
